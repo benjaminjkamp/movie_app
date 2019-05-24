97a780ac9bea5f86f4d625331de7ea70af68ca74
benjaminjkamp <mail@benjaminjkamp.com>
2019-05-23 23:32:08 -0500

    adds genres and associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,18 @@
 class Movie < ApplicationRecord
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
+
+  def genre_names
+    genre_names = []
+    movie_genres = MovieGenre.where(movie_id: id)
+    movie_genres.each do |movie_genre|
+      genre_names << movie_genre.genre.name
+    end
+    return genre_names
+  end
   
   validates :title, :director, :year, presence: true
   validates :year, numericality: { less_than: 2020, greater_than: 1800 }
--- app/views/api/movies/_movie.json.jbuilder
+++ app/views/api/movies/_movie.json.jbuilder
@@ -1,5 +1,6 @@
 json.title movie.title
 json.year movie.year
+json.genres movie.genre_names
 json.director movie.director
 json.plot movie.plot
 json.english movie.english
